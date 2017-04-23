9fe67956bb695af037fae273a6195e9bf6d2a9e9
Andrew Graus <agraus@gplogin3.local>
2017-04-23 13:19:54 -0700

    updated run_split_prog.py for generality

--- particle_manipulation_files/MUSIC_split_files/run_split_prog.py
+++ particle_manipulation_files/MUSIC_split_files/run_split_prog.py
@@ -9,7 +9,21 @@
 
 import os, sys, re, subprocess
 
+if len(sys.argv) < 3:
+    print 'usage: python run_split_prog.py <directory files are in> <number of species 4,5,6>'
+
+total_part = sys.argv[2]
+
 split_files=[]
+
+if total_part==4:
+    path_to_split = '/home/agraus/code/Group-tools/particle_manipulation_files/MUSIC_split_files/split_gb.py'
+elif total_part==5:
+    path_to_split = '/home/agraus/code/Group-tools/particle_manipulation_files/MUSIC_split_files/split_gb_5to4.py'
+elif total_part==6:
+    path_to_split = '/home/agraus/code/Group-tools/particle_manipulation_files/MUSIC_split_files/split_gb_6to4.py'
+elif total_part==7:
+    path_to_split = '/home/agraus/code/Group-tools/particle_manipulation_files/MUSIC_split_files/split_gb_7to4.py'
 
 #loop over files in directory and check if they are
 #gadget binaries
@@ -25,7 +39,7 @@ for import_file in split_files:
     im_file_split = re.split('[.]',import_file)
     #if im_file_split[-1]=='gadget' and import_file not in split_files:
     if im_file_split[-2] != 'split' and im_file_split[-2]+'.split.gadget' not in os.listdir(sys.argv[1]):
-        subprocess.call(['python','/home/agraus/code/python_generalscripts/split_gb_5to4.py',str(sys.argv[1])+import_file,str(sys.argv[1])+im_file_split[0]+'.split.gadget'])
+        subprocess.call(['python',path_to_split,str(sys.argv[1])+import_file,str(sys.argv[1])+im_file_split[0]+'.split.gadget'])
 
 
         
